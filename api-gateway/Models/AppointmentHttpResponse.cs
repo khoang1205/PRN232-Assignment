0b8c894c51819094aff7f60baa874431c0e951b0
vuong <tranducvuong31012003@gmail.com>
2025-07-21 23:23:17 +0700

    refactor 3

--- api-gateway/Models/AppointmentHttpResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace api_gateway.Models
-{
-    public class AppointmentHttpResponse
-    {
-        public string AppointmentId { get; set; } = default!;
-        public string Status { get; set; } = default!;
-    }
-}
--- api-gateway/Models/CreateAppointmentRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace api_gateway.Models
-{
-    public class CreateAppointmentRequest
-    {
-        public string DoctorId { get; set; } = default!;
-        public string PatientId { get; set; } = default!;
-        public string TimeSlot { get; set; } = default!;
-    }
-}
