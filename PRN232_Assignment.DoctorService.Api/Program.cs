2bfcac38865ccee1c238adce9179f9a0a28402a3
vuong <tranducvuong31012003@gmail.com>
2025-07-25 22:24:24 +0700

    fix doctor service in docker compose

--- PRN232_Assignment.DoctorService.Api/Program.cs
+++ PRN232_Assignment.DoctorService.Api/Program.cs
@@ -36,11 +36,8 @@ builder.Services.AddScoped<IMongoDatabase>(sp =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
